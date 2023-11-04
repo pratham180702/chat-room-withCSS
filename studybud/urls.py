4b12621b1d998a35448efed1154b4705c30e2a16
Pratham Kakkad <prathamkakkad18@gmail.com>
2023-11-04 11:57:38 +0530

    final

--- studybud/urls.py
+++ studybud/urls.py
@@ -1,9 +1,11 @@
-
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('base.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("base.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
